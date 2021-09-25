b296d474dca7949044e8e7212966d889baa240d7
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-09-25 15:30:48 +0100

    add frames

---
+++ src/Homepage/Navigation.tsx
@@ -0,0 +1,11 @@
+function Navigation() {
+  return (
+    <div>
+      <a href="https://www.linkedin.com/in/rosie-o-39a374a8/">Linked In</a>
+      <a href="https://twitter.com/_roseodonnell">Twitter</a>
+      <a href="https://github.com/Rosie-ODonnell">GitHub</a>
+    </div>
+  );
+}
+
+export default Navigation;
---
+++ src/Homepage/main.tsx
@@ -0,0 +1,17 @@
+import Navigation from "./Navigation";
+import "./homepage.css";
+
+function Main() {
+  return (
+    <div>
+      <div className="main__circle outer"></div>
+      <div className="main__circle inner"></div>
+
+      <h1>ROSIE O'DONNELL</h1>
+      <h2>SOFTWARE ENGINEER</h2>
+      <Navigation />
+    </div>
+  );
+}
+
+export default Main;
