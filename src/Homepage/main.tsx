da55fc22baa21b00dba0a41d7e53d111419062ad
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-09-25 15:48:11 +0100

    adding styling

--- src/Homepage/main.tsx
+++ src/Homepage/main.tsx
@@ -7,9 +7,11 @@ function Main() {
       <div className="main__circle outer"></div>
       <div className="main__circle inner"></div>
 
-      <h1>ROSIE O'DONNELL</h1>
-      <h2>SOFTWARE ENGINEER</h2>
-      <Navigation />
+      <div className="main">
+        <h1>ROSIE O'DONNELL</h1>
+        <h2>SOFTWARE ENGINEER</h2>
+        <Navigation />
+      </div>
     </div>
   );
 }
