f1a30169e064fbb6c6c4dcf76ce56c89c85f2919
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-09-26 19:05:17 +0100

    adding styling, icons

--- src/Homepage/Navigation.tsx
+++ src/Homepage/Navigation.tsx
@@ -1,9 +1,20 @@
+import "./homepage.css";
+import Github from "./GitHub.png";
+import LinkedIn from "./LI.png";
+import Twitter from "./Twitter.png";
+
 function Navigation() {
   return (
-    <div>
-      <a href="https://www.linkedin.com/in/rosie-o-39a374a8/">Linked In</a>
-      <a href="https://twitter.com/_roseodonnell">Twitter</a>
-      <a href="https://github.com/Rosie-ODonnell">GitHub</a>
+    <div className="navigation">
+      <a href="https://www.linkedin.com/in/rosie-o-39a374a8/">
+        <img src={LinkedIn} alt="linked in logo" id="li" />
+      </a>
+      <a href="https://twitter.com/_roseodonnell">
+        <img src={Twitter} alt="twitter logo" id="twitter" />
+      </a>
+      <a href="https://github.com/Rosie-ODonnell">
+        <img src={Github} alt="github logo" />
+      </a>
     </div>
   );
 }
--- src/Homepage/main.tsx
+++ src/Homepage/main.tsx
@@ -7,7 +7,7 @@ function Main() {
       <div className="main__circle outer"></div>
       <div className="main__circle inner"></div>
 
-      <div className="main">
+      <div className="titles">
         <h1>ROSIE O'DONNELL</h1>
         <h2>SOFTWARE ENGINEER</h2>
         <Navigation />
