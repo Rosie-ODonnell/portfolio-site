005e5c22ed303fa1da16d6ed0a3145992b566c3d
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-09-27 21:52:24 +0100

    re-design, circle removal, script text inserted

---
+++ src/Homepage/Script.tsx
@@ -0,0 +1,16 @@
+import Navigation from "./Navigation";
+
+function Script() {
+  return (
+    <div className="script">
+      <p className="line">Act 1</p>
+      <p className="line">Scene 1: London. A home office.</p>
+      <p className="line italic">(Enter Rosie O'Donnell)</p>
+      <p className="line">ROSIE:</p>
+      <p className="line">Hey, I'm a fullstack developer</p>
+      <Navigation />
+    </div>
+  );
+}
+
+export default Script;
--- src/Homepage/main.tsx
+++ src/Homepage/main.tsx
@@ -1,17 +1,10 @@
-import Navigation from "./Navigation";
+import Script from "./Script";
 import "./homepage.css";
 
 function Main() {
   return (
-    <div>
-      <div className="main__circle outer"></div>
-      <div className="main__circle inner"></div>
-
-      <div className="titles">
-        <h1>Rosie O'Donnell</h1>
-        <h2>Software Engineer</h2>
-        <Navigation />
-      </div>
+    <div className="center">
+      <Script />
     </div>
   );
 }
