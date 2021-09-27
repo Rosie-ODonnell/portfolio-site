167fe1cdb21b7d656049788b5bdb4ffefde83010
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-09-27 22:33:24 +0100

    deploying

--- src/Homepage/Script.tsx
+++ src/Homepage/Script.tsx
@@ -3,8 +3,8 @@ import Navigation from "./Navigation";
 function Script() {
   return (
     <div className="script">
-      <p className="line">Act 1</p>
-      <p className="line">Scene 1: London. A home office.</p>
+      <p className="line__1">Act 1</p>
+      <p className="line__2">Scene 1: London. A home office.</p>
       <p className="line italic">(Enter Rosie O'Donnell)</p>
       <p className="line">ROSIE:</p>
       <p className="line">Hey, I'm a fullstack developer</p>
