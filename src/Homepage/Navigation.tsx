bed7c433e6da3c9b0e889c4a982740056486b7ed
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-09-28 22:42:49 +0100

    img

--- src/Homepage/Navigation.tsx
+++ src/Homepage/Navigation.tsx
@@ -1,17 +1,19 @@
 import "./homepage.css";
 
+import Twitter from "./Twitter.svg";
+
 function Navigation() {
   return (
     <div className="navigation">
       <a href="https://www.linkedin.com/in/rosie-o-39a374a8/">
-        <img src="./images/LI.png" alt="linked in logo" id="li" />
+        <img src="/images/LI.png" alt="linked in logo" id="li" />
       </a>
 
       <a href="https://github.com/Rosie-ODonnell">
-        <img src="./images/GitHub.png" alt="github logo" />
+        <img src="/images/GitHub.png" alt="github logo" />
       </a>
       <a href="https://twitter.com/_roseodonnell" id="twitter">
-        <img src="images/Twitter.png" alt="twitter logo" id="twitter" />
+        <img src={Twitter} alt="twitter logo" id="twitter" />
       </a>
     </div>
   );
