127695b76fa09528fe19fb8463db7a6f08b059b8
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-09-28 13:29:35 +0100

    fix broken media queries and make text size smaller

--- src/Homepage/Script.tsx
+++ src/Homepage/Script.tsx
@@ -3,7 +3,7 @@ import Navigation from "./Navigation";
 function Script() {
   return (
     <div className="script">
-      <p className="line__1">Act 1</p>
+      <p className="line__1 ">Act 1</p>
       <p className="line__2">Scene 1: London. A home office.</p>
       <p className="line italic">(Enter Rosie O'Donnell)</p>
       <p className="line">ROSIE:</p>
