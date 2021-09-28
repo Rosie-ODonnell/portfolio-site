7642e9bbb9f816bdeec8f4be52413b41eee6defd
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-09-28 20:24:50 +0100

    moving images to public file

--- src/Homepage/Navigation.tsx
+++ src/Homepage/Navigation.tsx
@@ -1,20 +1,17 @@
 import "./homepage.css";
-import Github from "./GitHub.png";
-import LinkedIn from "./LI.png";
-import Twitter from "./Twitter.svg";
 
 function Navigation() {
   return (
     <div className="navigation">
       <a href="https://www.linkedin.com/in/rosie-o-39a374a8/">
-        <img src={LinkedIn} alt="linked in logo" id="li" />
+        <img src="./LI.png" alt="linked in logo" id="li" />
       </a>
 
       <a href="https://github.com/Rosie-ODonnell">
-        <img src={Github} alt="github logo" />
+        <img src="./GitHub.png" alt="github logo" />
       </a>
       <a href="https://twitter.com/_roseodonnell" id="twitter">
-        <img src={Twitter} alt="twitter logo" id="twitter" />
+        <img src="./Twitter.png" alt="twitter logo" id="twitter" />
       </a>
     </div>
   );
