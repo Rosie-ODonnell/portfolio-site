3134e1b7e083c59b0569d2ee99b630e63da27cce
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-09-28 20:37:35 +0100

    publishing images to react-app

--- src/Homepage/Navigation.tsx
+++ src/Homepage/Navigation.tsx
@@ -4,14 +4,14 @@ function Navigation() {
   return (
     <div className="navigation">
       <a href="https://www.linkedin.com/in/rosie-o-39a374a8/">
-        <img src="./LI.png" alt="linked in logo" id="li" />
+        <img src={"./LI.png"} alt="linked in logo" id="li" />
       </a>
 
       <a href="https://github.com/Rosie-ODonnell">
-        <img src="./GitHub.png" alt="github logo" />
+        <img src={"./GitHub.png"} alt="github logo" />
       </a>
       <a href="https://twitter.com/_roseodonnell" id="twitter">
-        <img src="./Twitter.png" alt="twitter logo" id="twitter" />
+        <img src={"./Twitter.png"} alt="twitter logo" id="twitter" />
       </a>
     </div>
   );
