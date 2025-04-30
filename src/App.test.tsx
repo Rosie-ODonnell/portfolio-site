054f55ac0cbbf490fba1c6e9295bf82c6553ef6f
Rosie O'Donnell <rosie.odonnell@talk21.com>
2025-04-30 10:46:26 +0100

    update jest to vitest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import App from "./App";
+import {test} from 'vitest'
+import {render} from '@testing-library/react';
 
-test("renders learn react link", () => {
+test("renders app", () => {
   render(<App />);
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  test: {
+    environment: 'jsdom'
+  },
   server: {
     open: true,
     port: 3000,
   },
+
 })
